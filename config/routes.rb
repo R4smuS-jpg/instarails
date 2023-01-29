01f315c971976e2d4794d2306a733802f411f920
R4smuS-jpg <r4smusjpg@gmail.com>
2023-01-29 23:16:07 +0300

    add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root path
   root 'users#index'
   
+  # users
   resources :users
+
+  # sessions
+  resources :sessions, only: %i[new create destroy]
 end
