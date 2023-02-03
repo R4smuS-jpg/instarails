63fd95a9a89f1ec403b1a8f029c1e3eaf2f53d30
R4smuS-jpg <r4smusjpg@gmail.com>
2023-02-03 21:26:27 +0300

    fix CreatePosts migration

--- db/migrate/20230203105942_create_posts.rb
+++ db/migrate/20230203105942_create_posts.rb
@@ -1,12 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :user_id, null: false
       t.string :content, null: true
 
       t.timestamps
     end
 
-    add_reference :posts, :users, foreign_key: true
+    add_reference :posts, :user, foreign_key: true
   end
 end
