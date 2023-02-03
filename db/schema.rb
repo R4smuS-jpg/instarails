66afa03aac3caee2d4fa6fc0aa2d929c4bd1f69f
R4smuS-jpg <r4smusjpg@gmail.com>
2023-02-03 14:24:56 +0300

    run CreatePosts migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_03_105637) do
+ActiveRecord::Schema.define(version: 2023_02_03_105942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2023_02_03_105637) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.citext "email", null: false
     t.string "nickname", null: false
@@ -57,4 +66,5 @@ ActiveRecord::Schema.define(version: 2023_02_03_105637) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users", column: "users_id"
 end
