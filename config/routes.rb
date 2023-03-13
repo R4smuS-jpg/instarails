ef28a8fb438fc1d9ae3cfd201f40189c7edcf9ab
R4smuS-jpg <r4smusjpg@gmail.com>
2023-03-13 16:22:17 +0300

    added routes for likes

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,12 @@ Rails.application.routes.draw do
                              edit
                              update
                              destroy] do
-    # comments
+    # post comments
     resources :comments, only: %i[edit create update destroy]
+
+    # post likes
+    post '/like', to: 'likes#create'
+    delete '/unlike', to: 'likes#destroy'
   end
 
   # followings
