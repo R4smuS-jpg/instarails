d9972be9983718c749e8713adaf39386527efa03
R4smuS-jpg <r4smusjpg@gmail.com>
2023-03-13 16:17:31 +0300

    added :with_likes scope to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,16 @@ class Post < ApplicationRecord
   # relations
   belongs_to :user
   has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
   has_many_attached :images, dependent: :delete_all
 
   # scopes
   scope :with_user_with_attached_avatar,
     -> { includes(:user).merge(User.with_attached_avatar) }
   scope :with_attached_images, -> { includes(images_attachments: :blob) }
+  scope :with_likes, -> { includes(:likes) }
   scope :with_comments_with_user_with_attached_avatar,
     -> { includes(:comments).merge(Comment.with_user_with_attached_avatar) }
-
   scope :by_created_at, ->(order) { order(created_at: order) }
 
   # validations
@@ -24,5 +25,9 @@ class Post < ApplicationRecord
   # instance methods
   def created_at_formatted
     self.created_at.strftime("%d/%m/%Y at %H:%M")
+  end
+
+  def liked_by_user?(user)
+    self.likes.find_by(user_id: user.id)
   end
 end
