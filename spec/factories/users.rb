4a5c6502d20384811704c76c2307fd2c8343f9b2
R4smuS-jpg <r4smusjpg@gmail.com>
2023-03-14 14:48:55 +0300

    added more users to users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,27 @@
 FactoryBot.define do
-  factory :user do
+  factory :user_1 do
     email  { FFaker::Internet.email }
-    nickname { FFaker::Internet.user_name }
-    full_name { 'User Userov' }
-    biography {'I am User =)'}
+    nickname { "#{FFaker::Internet.user_name}123" }
+    full_name { 'User Userov 1' }
+    biography {'I am User 1 =)'}
+    password { 'password' }
+    password_confirmation { 'password'}
+  end
+
+  factory :user_2 do
+    email  { FFaker::Internet.email }
+    nickname { "#{FFaker::Internet.user_name}123" }
+    full_name { 'User Userov 2' }
+    biography {'I am User 2 =)'}
+    password { 'password' }
+    password_confirmation { 'password'}
+  end
+
+  factory :user_3 do
+    email  { FFaker::Internet.email }
+    nickname { "#{FFaker::Internet.user_name}123" }
+    full_name { 'User Userov 3' }
+    biography {'I am User 3 =)'}
     password { 'password' }
     password_confirmation { 'password'}
   end
