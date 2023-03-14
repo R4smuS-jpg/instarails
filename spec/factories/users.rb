abae614086c04ba32d27ba677697bc4edb4e58c8
R4smuS-jpg <r4smusjpg@gmail.com>
2023-03-14 14:51:07 +0300

    fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,27 +1,9 @@
 FactoryBot.define do
-  factory :user_1 do
+  factory :user do
     email  { FFaker::Internet.email }
     nickname { "#{FFaker::Internet.user_name}123" }
     full_name { 'User Userov 1' }
     biography {'I am User 1 =)'}
-    password { 'password' }
-    password_confirmation { 'password'}
-  end
-
-  factory :user_2 do
-    email  { FFaker::Internet.email }
-    nickname { "#{FFaker::Internet.user_name}123" }
-    full_name { 'User Userov 2' }
-    biography {'I am User 2 =)'}
-    password { 'password' }
-    password_confirmation { 'password'}
-  end
-
-  factory :user_3 do
-    email  { FFaker::Internet.email }
-    nickname { "#{FFaker::Internet.user_name}123" }
-    full_name { 'User Userov 3' }
-    biography {'I am User 3 =)'}
     password { 'password' }
     password_confirmation { 'password'}
   end
