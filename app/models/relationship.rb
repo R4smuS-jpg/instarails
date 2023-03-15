8d5ed25963284612aa5fd66c51c63b28286d90ed
R4smuS-jpg <r4smusjpg@gmail.com>
2023-03-15 12:20:28 +0300

    added user_cannot_follow_himeself validation to Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -7,4 +7,13 @@ class Relationship < ApplicationRecord
   validates :follower_id, presence: true
   validates :followed_id, presence: true
   validates_uniqueness_of :followed_id, :scope => [:follower_id]
+  validate :user_cannot_follow_himself
+
+  private
+
+  def user_cannot_follow_himself
+    if follower_id == followed_id
+      errors.add(:follower_id, 'follower_id can\'t be equal to followed_id')
+    end
+  end
 end
