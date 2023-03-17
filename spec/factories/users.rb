bdbdd152560b34af5f541d0ec4df27fb2d8aa2b5
R4smuS-jpg <r4smusjpg@gmail.com>
2023-03-17 13:50:10 +0300

    changed static values of some attributes of user factory to FFaker generated

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :user do
     email  { FFaker::Internet.email }
     nickname { "#{FFaker::Internet.user_name}123" }
-    full_name { 'User Userov 1' }
-    biography {'I am User 1 =)'}
-    password { 'password' }
-    password_confirmation { 'password'}
+    full_name { FFaker::Name.name }
+    biography { FFaker::Lorem.phrase }
+    password { 'password123' }
+    password_confirmation { 'password123' }
   end
 end
